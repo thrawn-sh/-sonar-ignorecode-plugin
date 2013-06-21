46ba07752add5c479ee1c6079c34fa473619e83a
Alexander Dreweke <thrawn@shadowhunt.de>
2013-06-21 23:28:00 +0200

    more tests

--- src/test/java/de/shadowhunt/sonar/plugins/ignorecode/batch/IgnoreMissingCoverageDecoratorTest.java
+++ src/test/java/de/shadowhunt/sonar/plugins/ignorecode/batch/IgnoreMissingCoverageDecoratorTest.java
@@ -9,24 +9,46 @@ import java.util.Set;
 import junit.framework.Assert;
 
 import org.apache.commons.configuration.Configuration;
+import org.apache.ibatis.session.SqlSession;
 import org.apache.maven.project.MavenProject;
-import org.junit.Ignore;
 import org.junit.Test;
+import org.mockito.Matchers;
 import org.mockito.Mockito;
 import org.sonar.api.batch.DecoratorContext;
+import org.sonar.api.database.model.MeasureMapper;
+import org.sonar.api.database.model.Snapshot;
 import org.sonar.api.measures.CoreMetrics;
 import org.sonar.api.measures.Measure;
 import org.sonar.api.resources.Directory;
 import org.sonar.api.resources.Java;
 import org.sonar.api.resources.JavaFile;
 import org.sonar.api.resources.Project;
+import org.sonar.api.resources.Resource;
 import org.sonar.api.utils.SonarException;
 import org.sonar.batch.index.MeasurePersister;
+import org.sonar.batch.index.MemoryOptimizer;
+import org.sonar.batch.index.ResourcePersister;
+import org.sonar.core.persistence.MyBatis;
 
 public class IgnoreMissingCoverageDecoratorTest {
 
+	private static MeasurePersister createMeasurePersisterMock() {
+		final ResourcePersister resourcePersister = Mockito.mock(ResourcePersister.class);
+		final Snapshot snapshot = Mockito.mock(Snapshot.class);
+		Mockito.when(resourcePersister.getSnapshotOrFail(Matchers.<Resource<?>> any())).thenReturn(snapshot);
+
+		final MyBatis mybatis = Mockito.mock(MyBatis.class);
+		final SqlSession session = Mockito.mock(SqlSession.class);
+		Mockito.when(mybatis.openSession()).thenReturn(session);
+		final MeasureMapper mapper = Mockito.mock(MeasureMapper.class);
+		Mockito.when(session.getMapper(MeasureMapper.class)).thenReturn(mapper);
+
+		final MemoryOptimizer optimizer = Mockito.mock(MemoryOptimizer.class);
+
+		return new MeasurePersister(mybatis, resourcePersister, null, optimizer);
+	}
+
 	@Test
-	@Ignore
 	public void decorate() throws IOException {
 		final File tempFile = File.createTempFile("coverage-", ".tmp");
 		final PrintWriter writer = new PrintWriter(tempFile);
@@ -36,14 +58,38 @@ public class IgnoreMissingCoverageDecoratorTest {
 		final Configuration configuration = Mockito.mock(Configuration.class);
 		Mockito.when(configuration.getString(IgnoreMissingCoverageDecorator.CONFIG_FILE)).thenReturn(tempFile.getPath());
 
-		final MeasurePersister persister = new MeasurePersister(null, null, null, null);
+		final MeasurePersister persister = createMeasurePersisterMock();
 		final IgnoreMissingCoverageDecorator decorator = new IgnoreMissingCoverageDecorator(persister, configuration);
 
 		final DecoratorContext context = Mockito.mock(DecoratorContext.class);
 		Mockito.when(context.getMeasure(CoreMetrics.COVERAGE_LINE_HITS_DATA)).thenReturn(new Measure());
 
+		final Measure cbl = new Measure(CoreMetrics.CONDITIONS_BY_LINE, "3=1;6=2");
+		Mockito.when(context.getMeasure(CoreMetrics.CONDITIONS_BY_LINE)).thenReturn(cbl);
+		final Measure ctc = new Measure(CoreMetrics.CONDITIONS_TO_COVER, 3.0);
+		Mockito.when(context.getMeasure(CoreMetrics.CONDITIONS_TO_COVER)).thenReturn(ctc);
+		final Measure ccbl = new Measure(CoreMetrics.COVERED_CONDITIONS_BY_LINE, "6=1");
+		Mockito.when(context.getMeasure(CoreMetrics.COVERED_CONDITIONS_BY_LINE)).thenReturn(ccbl);
+		final Measure uc = new Measure(CoreMetrics.UNCOVERED_CONDITIONS, 2.0);
+		Mockito.when(context.getMeasure(CoreMetrics.UNCOVERED_CONDITIONS)).thenReturn(uc);
+
+		final Measure clh = new Measure(CoreMetrics.COVERAGE_LINE_HITS_DATA, "1=1;2=0;3=0;4=0;5=0;6=1");
+		Mockito.when(context.getMeasure(CoreMetrics.COVERAGE_LINE_HITS_DATA)).thenReturn(clh);
+		final Measure ltc = new Measure(CoreMetrics.LINES_TO_COVER, 6.0);
+		Mockito.when(context.getMeasure(CoreMetrics.LINES_TO_COVER)).thenReturn(ltc);
+		final Measure lc = new Measure(CoreMetrics.LINE_COVERAGE, 2.0);
+		Mockito.when(context.getMeasure(CoreMetrics.LINE_COVERAGE)).thenReturn(lc);
+
 		final JavaFile file = new JavaFile("net.example.foo.Bar");
 		decorator.decorate(file, context);
+		Assert.assertEquals("CONDITIONS_BY_LINE must match", "6=2", cbl.getData());
+		Assert.assertEquals("CONDITIONS_TO_COVER must match", 2.0, ctc.getValue());
+		Assert.assertEquals("COVERED_CONDITIONS_BY_LINE must match", "6=1", ccbl.getData());
+		Assert.assertEquals("UNCOVERED_CONDITIONS must match", 1.0, uc.getValue());
+
+		Assert.assertEquals("COVERAGE_LINE_HITS_DATA must match", "1=1;6=1", clh.getData());
+		Assert.assertEquals("LINES_TO_COVER must match", 2.0, ltc.getValue());
+		Assert.assertEquals("LINE_COVERAGE must match", 2.0, lc.getValue());
 	}
 
 	@Test
